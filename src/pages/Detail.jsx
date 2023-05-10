8dcb937098513a7adb827d6c28f117a18d8c78a8
Andry Pebrianto <andrypeb227@gmail.com>
2023-05-11 01:18:04 +0700

    Memperbaiki error dari eslint

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { IoChevronBackOutline } from "react-icons/io5";
 import { AiOutlineCheck, AiOutlinePlus } from "react-icons/ai";
