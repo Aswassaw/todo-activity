109da893c60ecc21f18aebf221a444b30f42faf9
Andry Pebrianto <andrypeb227@gmail.com>
2023-05-11 05:19:18 +0700

    Modal delete

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -11,46 +11,43 @@ export default function DeleteModal({
 }) {
   return (
     <Modal
-      data-cy="modal-delete"
       aria-labelledby="contained-modal-title-vcenter"
       centered
       show={deleteModal}
       onHide={showDeleteModal}
     >
-      <div className="modal-content">
-        <div className="modal-body p-5 row d-flex">
-          <p
-            data-cy="modal-delete-icon"
-            className="display-2 text-center text-danger"
+      <div data-cy="modal-delete" className="modal-body p-5 row d-flex">
+        <p
+          data-cy="modal-delete-icon"
+          className="display-2 text-center text-danger"
+        >
+          <HiOutlineExclamationTriangle />
+        </p>
+        <p data-cy="modal-delete-title" className="text-center fs-5">
+          {text} “<span className="fw-bold">{item && item.title}</span>
+          ”?
+        </p>
+        <div className="d-flex justify-content-between">
+          <button
+            style={{ fontSize: "18px" }}
+            type="button"
+            data-cy="modal-delete-cancel-button"
+            className="btn btn-custom btn-light py-3 px-4 fw-bold rounded-pill"
+            onClick={() => showDeleteModal(false)}
           >
-            <HiOutlineExclamationTriangle />
-          </p>
-          <p data-cy="modal-delete-title" className="text-center fs-5">
-            {text} “<span className="fw-bold">{item && item.title}</span>
-            ”?
-          </p>
-          <div className="d-flex justify-content-between">
-            <button
-              style={{ fontSize: "18px" }}
-              type="button"
-              data-cy="modal-delete-cancel-button"
-              className="btn btn-custom btn-light py-3 px-4 fw-bold rounded-pill"
-              onClick={() => showDeleteModal(false)}
-            >
-              Batal
-            </button>
-            <button
-              style={{ fontSize: "18px" }}
-              type="button"
-              data-cy="modal-delete-confirm-button"
-              className="btn btn-custom btn-red text-white py-3 px-4 fw-bold rounded-pill"
-              onClick={() => {
-                deleteAction(item.id);
-              }}
-            >
-              Hapus
-            </button>
-          </div>
+            Batal
+          </button>
+          <button
+            style={{ fontSize: "18px" }}
+            type="button"
+            data-cy="modal-delete-confirm-button"
+            className="btn btn-custom btn-red text-white py-3 px-4 fw-bold rounded-pill"
+            onClick={() => {
+              deleteAction(item.id);
+            }}
+          >
+            Hapus
+          </button>
         </div>
       </div>
     </Modal>
