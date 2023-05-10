e9c7f5f43efeb94856909a2a160241c65ca5f452
Andry Pebrianto <andrypeb227@gmail.com>
2023-05-11 01:57:43 +0700

    Perbaikan Layout: Dapat menampilkan item list atau empty state Poin 15

--- src/components/ActivityList.jsx
+++ src/components/ActivityList.jsx
@@ -10,9 +10,9 @@ export default function ActivityList({ activities, deleteActivity }) {
 
   return (
     <div className="row row-cols-1 row-cols-md-4 g-4 mt-4">
-      {activities.map((activity, index) => (
+      {activities.map((activity) => (
         <div
-          data-cy={`activity-item-${index}`}
+          data-cy={`activity-item`}
           key={activity.id}
           className="col"
         >
