9b61c44f6370e4b2156f6379f9d90ba63526a637
Andry Pebrianto <andrypeb227@gmail.com>
2023-05-11 02:57:14 +0700

    Meletakkan modal di luar row agar tidak error

--- src/components/ActivityList.jsx
+++ src/components/ActivityList.jsx
@@ -9,39 +9,45 @@ export default function ActivityList({ activities, deleteActivity }) {
   const [selectedActivity, setSelectedActivity] = useState(null);
 
   return (
-    <div className="row row-cols-1 row-cols-md-4 g-4 mt-4">
-      {activities.map((activity) => (
-        <div data-cy={`activity-item`} key={activity.id} className="col">
-          <div className="card h-100 shadow border-0 p-2">
-            <div className="card-body">
-              <h5 className="card-title fw-bold" data-cy="activity-item-title">
-                <Link to={`/detail/${activity.id}`}>{activity.title}</Link>
-              </h5>
-              <div className="card-text d-flex justify-content-between mt-10">
-                <div>
-                  <p data-cy="activity-item-date">
-                    {moment(activity.created_at).format("D MMMM YYYY")}
-                  </p>
+    <>
+      <div className="row row-cols-1 row-cols-md-4 g-4 mt-4">
+        {activities.map((activity) => (
+          <div data-cy={`activity-item`} key={activity.id} className="col">
+            <div className="card h-100 shadow border-0 p-2">
+              <div className="card-body">
+                <h5
+                  className="card-title fw-bold"
+                  data-cy="activity-item-title"
+                >
+                  <Link to={`/detail/${activity.id}`}>{activity.title}</Link>
+                </h5>
+                <div className="card-text d-flex justify-content-between mt-10">
+                  <div>
+                    <p data-cy="activity-item-date">
+                      {moment(activity.created_at).format("D MMMM YYYY")}
+                    </p>
+                  </div>
+                  <div
+                    className="pointer"
+                    onClick={() => setSelectedActivity(activity)}
+                    data-cy="activity-item-delete-button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#deleteModal"
+                  >
+                    <RiDeleteBin6Line className="fs-5" />
+                  </div>
                 </div>
-                <RiDeleteBin6Line
-                  data-cy="activity-item-delete-button"
-                  className="pointer fs-5"
-                  onClick={() => {
-                    setSelectedActivity(activity);
-                    document.getElementById("showDeleteModal").click();
-                  }}
-                />
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
 
       <DeleteModal
         text="Apakah anda yakin menghapus activity"
         item={selectedActivity}
         deleteAction={deleteActivity}
       />
-    </div>
+    </>
   );
 }
